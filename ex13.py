073e963b4fb9ec1b1c42ad7405e95f93ca17406e
in-ura <inura.eu@gmail.com>
2013-10-01 15:43:26 +0200

    trettende oppgaven

---
+++ ex13.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "your first variavle is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+# for å kjøre må du bruke .\ex13.py i powershell
