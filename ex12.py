20ddad68b7c7c5285e7af0d9824ad6cf847382ec
in-ura <inura.eu@gmail.com>
2013-10-01 14:44:44 +0200

    tolvte oppgave

---
+++ ex12.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+Y = raw_input("Name? ")
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "So, you're %r old, %r tall and %r heavy and your name is %r." % (age, height, weight, Y) 
